1a1b1a4091f440c6d31b75d73e1bb31a0246f41a
whle1ouch <282217645@qq.com>
2022-07-22 16:56:42 +0800

    swinv1:update

--- pytorch/swinTransformer/swin.py
+++ pytorch/swinTransformer/swin.py
@@ -41,11 +41,12 @@ class WindowAttention(nn.Module):
         relative_coords[:, :, 0] *= 2 * window_size - 1
         relative_position_index = relative_coords.sum(-1)
         self.register_buffer("relative_position_index", relative_position_index)
+        self.softmax = nn.Softmax(dim=-1)
 
         
         
         
-    def forward(self, x, mask = None):
+    def forward(self, x, mask=None):
         bs, sql_len, model_dim = x.shape
         x_proj = self.attn_proj(x)
         q, k, v = x_proj.chunk(3, dim=-1)
@@ -66,126 +67,141 @@ class WindowAttention(nn.Module):
             self.window_size * self.window_size, self.window_size * self.window_size, -1
         )
         relative_position_bias = relative_position_bias.permute(2, 0, 1).contiguous().tile(bs, 1,  1)
+        
+        attn = torch.bmm(q, k.transpose(-1, -2)) / math.sqrt(head_dim) + relative_position_bias 
         if mask is None:
-            attn_prob = F.softmax(torch.bmm(q, k.transpose(-1, -2)) / math.sqrt(head_dim) + relative_position_bias, dim=-1) # sql_len * model_dim
+            attn_prob = self.softmax(attn)
         else:
-            additive_mask = mask.tile((num_head, 1, 1))
-        
-            attn_prob = F.softmax(torch.bmm(q, k.transpose(-1, -2)) / math.sqrt(head_dim) + additive_mask + relative_position_bias, dim=-1)
-        
+            num_windows = mask.shape[0]
+            b_ = bs // num_windows
+            mask = mask.tile(b_*num_head, 1, 1)
+            attn = attn + mask
+            attn_prob = self.softmax(attn) 
+           
         out = torch.bmm(attn_prob, v)
         out = out.reshape((bs, num_head, sql_len, head_dim)).transpose(1, 2)
         out = out.reshape((bs, sql_len, model_dim))
         out = self.linear(out)
-        return attn_prob, out
+        return out
     
    
 class PatchMerging(nn.Module):
     
-    def __init__(self, model_dim, merge_size, output_depth_scale=0.5) -> None:
+    def __init__(self, input_shape, model_dim, merge_size, output_depth_scale=0.5) -> None:
         super().__init__()
         self.merge_size = merge_size
+        self.input_shape = input_shape
         self.proj = nn.Linear(model_dim * merge_size * merge_size, int(model_dim * merge_size * merge_size * output_depth_scale))
         
     def forward(self, x):
-        bs, patch_num, model_dim = x.shape
+        bs, length, model_dim = x.shape
+        height, width = self.input_shape
+        assert length == height * width, "erorr"
         merge_size = self.merge_size
-        patch_size = int(math.sqrt(patch_num))
-        x = x.transpose(-2, -1).reshape((bs, model_dim, patch_size, patch_size))
+        x = x.transpose(-2, -1).reshape((bs, model_dim, height, width))
         x = F.unfold(x, (merge_size, merge_size), stride=(merge_size, merge_size)).transpose(-1, -2) # bs,  (patch_num / (merge_size**2)),  model_dim * (merge_size**2)
         x = self.proj(x) # bs,  (patch_num / (merge_size**2)), model_dim * merge_size * merge_size * output_depth_scale 
         return x 
     
     
-class SuccessiveSwinTransfomerBlock(nn.Module):
+class TransformerBlock(nn.Module):
     
-    def __init__(self, model_dim, num_head,  window_size) -> None:
+    def __init__(self, input_shape, model_dim, num_head, window_size, shift_size, mlp_ratio, dropout_rate) -> None:
         super().__init__()
-        
+        assert len(input_shape) == 2, "input image shape error"
+        assert 0 <= shift_size < window_size ,"shift size mush less than window size"
         self.window_size = window_size
-        
+        self.shift_size = shift_size
+        self.input_shape = input_shape
         self.ln1 = nn.LayerNorm(model_dim)
-        self.wmsa = WindowAttention(model_dim, num_head, window_size)
-        self.ln2 = nn.LayerNorm(model_dim)
-        self.mlp1 = nn.Linear(model_dim, model_dim)
-        
-        self.ln3 = nn.LayerNorm(model_dim)
         self.swmsa = WindowAttention(model_dim, num_head, window_size)
-        self.ln4 = nn.LayerNorm(model_dim)
-        self.mlp2 = nn.Linear(model_dim, model_dim)
+        self.ln2 = nn.LayerNorm(model_dim)
+    
+        mlp_hidden_dim = mlp_ratio * model_dim
+        self.mlp = nn.Sequential(
+            nn.Linear(model_dim, mlp_hidden_dim), 
+            nn.GELU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(mlp_hidden_dim, model_dim),
+            nn.Dropout(dropout_rate)
+            )
         
         
+        if self.shift_size > 0:
+            height, width = self.input_shape
+            image_mask = torch.zeros(1, height, width, 1)
+            h_slices = [
+                slice(0, -self.window_size),
+                slice(-self.window_size, -self.shift_size),
+                slice(-self.shift_size, None)
+            ]
+            w_slices = [
+                slice(0, -self.window_size),
+                slice(-self.window_size, -self.shift_size),
+                slice(-self.shift_size, None)
+            ]
+            cl = 0
+            for hs in h_slices:
+                for ws in w_slices:
+                    image_mask[:, hs, ws, :] = cl
+                    cl += 1
+            mask_windows = self.window_partition(image_mask)
+            mask_windows = mask_windows.view(-1, self.window_size * self.window_size)
+            attn_mask = mask_windows[:, None, :] - mask_windows[:, :, None]
+            attn_mask = attn_mask.masked_fill(attn_mask != 0, float(-100.)).masked_fill(attn_mask == 0, float(0.0))
+        else:
+            attn_mask = None
+        
+        self.register_buffer("attn_mask", attn_mask)
+            
         
     
     def forward(self, x):
-        bs, patch_num, model_dim = x.shape
-        window_size = self.window_size
-        patch_size = int(math.sqrt(patch_num))
-        window_num = self.get_window_num(patch_size)
-        
-        y = self.ln1(x)
-        y = self.sequence2image(y, window_size, window_num)
-        _, y = self.wmsa(y)
-        y = self.image2sequence(y, bs, window_num, window_size, model_dim)
-        y = x + y
-        
-        x = y 
-        y = self.ln2(x)
-        y = self.mlp1(y)
-        y = x + y
-        
-        x = y
-        y = self.ln3(x)
-        y = self.sequence2shift(y, window_size, window_num, patch_size)
-        mask = self.get_shift_mask(bs, window_size, window_num, patch_size)
-        _, y = self.swmsa(y, mask)
-        y = self.shift2sequence(y, bs, window_num, window_size, model_dim)
-        y = x + y
-        
-        x = y
-        y = self.ln4(x)
-        y = self.mlp2(y)
-        y + x + y
-        return y
-        
-        
-    def get_window_num(self, patch_size):
-        return patch_size // self.window_size
-    
-    @staticmethod
-    def image2sequence(x, batch_size, window_num, window_size, model_dim):
-        return x.reshape((batch_size, window_num * window_num * window_size * window_size, model_dim))
+        bs, length, model_dim = x.shape
+        height, width = self.input_shape
+        assert length == height * width, "error length != height * width"
 
-    @staticmethod
-    def shift2sequence(x, batch_size, window_num, window_size, model_dim):
-        x = x.reshape((batch_size, window_num, window_num, window_size, window_size, model_dim)).transpose(2, 3)
-        patch_size = window_num * window_size
-        shift_size = window_size // 2
-        x = x.reshape((batch_size, patch_size, patch_size, model_dim))
-        x = torch.roll(x, (shift_size, shift_size), dims=(1, 2))
-        return x.reshape(batch_size, patch_size * patch_size, model_dim)
+        shortcut = x 
+        x = self.ln1(x)
+        x = x.reshape(bs, height, width, model_dim)
+        if self.shift_size > 0:
+            x = torch.roll(x, (-self.shift_size, -self.shift_size), dims=(1, 2))
+            x = self.window_partition(x)
+        else:
+            x = self.window_partition(x)
+        x = x.reshape(-1, self.window_size * self.window_size, model_dim)
+        x = self.swmsa(x, self.attn_mask)
+        x = self.window_reverse(x)
         
-    @staticmethod
-    def sequence2image(x, window_size, window_num):
-        bs, _, model_dim = x.shape
-        x = x.reshape((bs, window_num, window_size, window_num, window_size, model_dim)).transpose(2, 3)
-        x = x.reshape((bs * window_num * window_num, window_size * window_size, model_dim))
+        if self.shift_size > 0:
+            x = torch.roll(x, (self.shift_size, self.shift_size), dims=(1, 2))
+        x = x.reshape(bs, length, model_dim)
+        
+        x = x + shortcut
+        shortcut = x
+        
+        x = self.ln2(x)
+        x = self.mlp(x)
+        x = x + shortcut
+        return x
+        
+    
+    def window_partition(self, x):
+        batch, height, width, channel = x.shape
+        x = x.reshape((batch, height // self.window_size, self.window_size, width // self.window_size, self.window_size, channel)).transpose(2, 3)
+        x = x.contiguous().view(-1, self.window_size, self.window_size, channel)
         return x
     
-    @staticmethod
-    def sequence2shift(x, window_size, window_num, patch_size):
-        bs, _, model_dim = x.shape
-        shift_size = window_size // 2
-        num_windows = window_num * window_num
-        num_patch_in_window = window_size * window_size
-        x = x.reshape((bs, patch_size, patch_size, model_dim))
-        x = torch.roll(x, (-shift_size, -shift_size), dims=(1, 2))
-        x = x.reshape((bs, window_num, window_size, window_num, window_size, model_dim)).transpose(2, 3)
-        x = x.reshape((bs * num_windows, num_patch_in_window, model_dim))
+    def window_reverse(self, x):
+        height, width = self.input_shape
+        batch_size = int(x.shape[0] /  (height * width / self.window_size / self.window_size))
+        x = x.reshape(batch_size, height // self.window_size, width // self.window_size, self.window_size, self.window_size, -1)
+        x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(batch_size, height, width, -1)
         return x
+
     
     @staticmethod
-    def get_shift_mask(batch_size, window_size, window_num, patch_size):
+    def get_attn_mask(window_size, window_num, patch_size):
         num_windows = window_num * window_num
         num_patch_in_window = window_size * window_size
         shift_size = window_size // 2
@@ -200,51 +216,94 @@ class SuccessiveSwinTransfomerBlock(nn.Module):
         index_mat = index_mat.reshape((1, window_num, window_size, window_num, window_size)).transpose(2, 3)
         
         c1 = index_mat.reshape((1, num_windows, num_patch_in_window)).unsqueeze(-1)
-        c1 = c1.tile(batch_size, 1, 1)
         c2 = (c1 - c1.transpose(-1, -2))  == 0
         c2 = c2.to(torch.float32)
         mask = (1 - c2) * (1e-9)
-        mask = mask.reshape((batch_size * num_windows, num_patch_in_window, num_patch_in_window))
+        mask = mask.reshape((num_windows, num_patch_in_window, num_patch_in_window))
         return mask
     
 
+class StageLayer(nn.Module):
+    
+    def __init__(self, depth, input_shape, model_dim, num_head, window_size, mlp_ratio, dropout_rate, downsample):
+        super().__init__() 
+        self.blocks = nn.ModuleList(
+            [TransformerBlock(
+                input_shape,
+                model_dim,
+                num_head,
+                window_size,
+                shift_size= 0 if i % 2 == 0 else window_size // 2,
+                mlp_ratio= mlp_ratio, 
+                dropout_rate=dropout_rate
+                ) for i in range(depth)]
+        )
+        
+        if downsample:
+            self.merge = PatchMerging(input_shape, model_dim, 2)
+        else:
+            self.merge = nn.Identity()
+    
+    def forward(self, x):
+        for block in self.blocks:
+            x = block(x)
+        x = self.merge(x)
+        return x
+        
+
 class SwinTransformer(nn.Module):
     
-    def __init__(self, patch_size, C, layer_numbers, num_head, window_size) -> None:
+    def __init__(self, image_shape, patch_size, C, depths, num_head, window_size, mlp_ratio=4, dropout_rate=0, num_classes=0) -> None:
         super().__init__()
-        assert len(layer_numbers) == 4
+        assert len(depths) == 4
         model_dim = C
         
         self.add_module("patch_embedding", PatchEmbedding(C, patch_size))
-        for i, num in enumerate(layer_numbers):
-            assert num % 2 == 0
-            successive_num = num // 2
-            ml = []
-            if i > 0:
-                ml.append(PatchMerging(model_dim, 2, 0.5))
+        input_shape = (image_shape[0] // patch_size, image_shape[1] // patch_size)
+        
+        for i, d in enumerate(depths):
+            self.add_module(f"starge{i+1}", StageLayer(
+                depth=d,
+                input_shape=input_shape,
+                model_dim=model_dim,
+                num_head=num_head,
+                window_size=window_size,
+                mlp_ratio=mlp_ratio,
+                dropout_rate=dropout_rate, 
+                downsample=True if i < len(depths)-1 else False
+                ))
+            if i < len(depths) -1:
                 model_dim *= 2
-            ml += [SuccessiveSwinTransfomerBlock(model_dim, num_head, window_size) for _ in range(successive_num)]
-            self.add_module(f"starge{i+1}", nn.Sequential(*ml))
-            
+                input_shape = (input_shape[0] // 2, input_shape[1] // 2)
+                
+        
+        if num_classes > 0:
+            head = nn.Sequential(nn.LayerNorm(model_dim),
+                                nn.AdaptiveAvgPool1d(1),
+                                nn.Linear(model_dim, num_classes))
+        else:
+            head = nn.Identity()
+        self.add_module("head", head)
+
     
     def forward(self, x):
         for name, m in self.named_children():
             x = m(x)
         return x
     
-def swin_t(patch_size, num_head, window_size):
-    return SwinTransformer(patch_size, 96, [2, 2, 6, 2], num_head, window_size)
+def swin_t(input_size, patch_size, num_head, window_size):
+    return SwinTransformer(input_size, patch_size, 96, [2, 2, 6, 2], num_head, window_size)
 
-def swin_s(patch_size, num_head, window_size):
-    return SwinTransformer(patch_size, 96, [2, 2, 18, 2], num_head, window_size)
+def swin_s(input_size, patch_size, num_head, window_size):
+    return SwinTransformer(input_size, patch_size, 96, [2, 2, 18, 2], num_head, window_size)
 
-def swin_b(patch_size, num_head, window_size):
-    return SwinTransformer(patch_size, 128, [2, 2, 18, 2], num_head, window_size)
+def swin_b(input_size, patch_size, num_head, window_size):
+    return SwinTransformer(input_size, patch_size, 128, [2, 2, 18, 2], num_head, window_size)
 
-def swin_l(patch_size, num_head, window_size):
-    return SwinTransformer(patch_size, 192, [2, 2, 18, 2], num_head, window_size)
+def swin_l(input_size, patch_size, num_head, window_size):
+    return SwinTransformer(input_size, patch_size, 192, [2, 2, 18, 2], num_head, window_size)
 
 
 if __name__ == "__main__":
-    model = swin_t(4, 2, 7)
+    model = swin_t((224, 224), 4, 2, 7)
     summary(model, (3, 224, 224))
